0c66767447c649cd0386421ddfaa89f7e8d11512
umbralcalc <umbralcalc@gmail.com>
2020-07-06 16:32:29 +0100

    Added kernel convolution and t-distribution logPDF functions/subroutines

--- source/bants.py
+++ source/bants.py
@@ -4,17 +4,16 @@ BANTS - BAyesian Networks for Time Series forecasting
 
 This is the main 'bants' class to be used on generic N-dimensional datasets. The structure is intended to be as simple
 as possible for rapid use in a commerical data science context. For more details on the mathematics behind 'bants'
-please refer to the notes/how_bants_works.ipynb Jupyter Notebook in the Git repository.
+please refer to the notes/how_bants_works.ipynb Jupyter Notebook in the Git repository. 
+
+Note that due to speed requirements, 'bants' takes as input only time series data that are measured at equally-spaced 
+time intervals - this is due to the fact that the kernel convolutions are much faster with a constant window function.
 
 '''
 
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-
-# Rename some modules
-tfp = tf.probability
-tfd = tfp.distributions
+import scipy.special as sps
 
 # Initialize the 'bants' method class
 class bants:
@@ -28,6 +27,7 @@ class bants:
         self.predict
         self.optimise_ARGP_hyperp
         self.kconv
+        self.tD_logpdf
         
         # Set network type
         self.net_type = net_type
@@ -39,13 +39,36 @@ class bants:
 
         # If network type is 'AR-GP' then set kernel types
         if self.net_type == 'AR-GP':
+            
             # Default type of convolution kernel for each column is always 'SquareExp'. The other option, for oscillatory 
             # columns in the data over time, is 'Periodic'. 
             self.column_kernel_types = ['SquareExp']
-
             
+            # Set signal periods in dimensions of the dataframe index (time variable chosen) - this should be a list of 
+            # the same length as the dimensions (columns) in the data where entries are relevant for 'Periodic' columns.
+            self.signal_periods = []
+
+    
+    # Function to output multivariate t-distribution (see here: https://en.wikipedia.org/wiki/Multivariate_t-distribution)
+    # log-Probability Density Function from input dataframe points. No scipy implementation so wrote this one.
+    def tD_logpdf(self,df,nu,mu,Sigma):
+        
+        # Compute the log normalisation of the distribution using scipy loggammas
+        log_norm = sps.loggamma((nu+self.Nd)/2.0) - sps.loggamma(nu/2.0) - \
+                   ((self.Nd/2.0)*np.log(np.pi*nu)) - (0.5*np.log(np.linalg.det(Sigma)))
+        
+        # Compute the log density function for each of the samples
+        x_minus_mu = df.values-np.tensordot(np.ones(self.Ns),mu,axes=0)
+        inverseSigma = np.linalg.inv(Sigma)
+        contraction = x_minus_mu*np.tensordot(inverseSigma,x_minus_mu,axes=([0],[1]))
+        log_densfunc = -((nu+self.Nd)/2.0)*np.log(1.0+(contraction/nu)) 
+        
+        # Output result
+        return log_norm + log_densfunc
+    
+
     # Kernel convolution for the 'AR-GP' network
-    def kconv(self,df,h):
+    def kconv(self,df,h,truncn=self.trunc_kconv_data):
     '''
     Method of kernel convolution on input dataframe values according to whichever kernel types were specified
     in the bants.column_kernel_types list. This is used mainly in bants.optimise_ARGP_hyperp but can be used
@@ -55,17 +78,34 @@ class bants:
     
     df       -     This is the input dataframe of values to perform the convolution on.
     
-    h        -     This is an input 1-d array of the same length as the number of columns in the dataframe.
+    h       -     This is an input 1-d array of the same length as the number of columns in the dataframe.
     
     OUTPUT:
     
     conv_d   -     This is an output array of convolved data the same shape as the input dataframe.     
                        
     '''
+        # Extract the change in time from the dataframe
+        delta_t = df.index[1]-df.index[0]
         
-        #conv_d = (self.column_kernel_types == 'SquareExp')*(df.values*np.exp(-df.index)) + \
-        #         (self.column_kernel_types == 'Periodic')*(df.values*np.exp())
+        # Generate an array of centred distances to be used to make the window array of the kernel
+        dtimes = delta_t*np.arange(0,self.Ns,1)
         
+        # Create function which returns an array corresponding to the convolution window function for
+        # the chosen input kernel type.
+        def kern_array(kern_type,h,perd):
+            
+            # Output weights corresponding to the convolution kernels
+            if kern_type == 'SquareExp': return np.exp(-dtimes**2.0/(2.0*(h**2.0)))
+            if kern_type == 'Periodic': return np.exp(-2.0*((np.sin(np.abs(np.pi*dtimes/perd)))**2.0)/(h**2.0))
+    
+        # Evaluate the convolution on the data dependent on the choice of kernel
+        conv_d = np.asarray([((self.column_kernel_types[i] == 'SquareExp')*np.convolve(df.values[:,i],\
+                 kern_array('SquareExp',h[i],self.signal_periods[i]))[:self.Ns]) + \
+                 ((self.column_kernel_types[i] == 'Periodic')*np.convolve(df.values[:,i],\
+                 kern_array('Periodic',h[i],self.signal_periods[i]))[:self.Ns]) for i in range(0,self.Nd)]).T
+        
+        # Return convolved signals
         return conv_d
             
             
@@ -77,7 +117,7 @@ class bants:
                        
     '''
         # Define the function to optimise over to obtain optimal network hyperparameters
-        def func_to_opt(params,df=self.train_df,N=self.N):
+        def func_to_opt(params,df=self.train_df,N=self.Nd):
             
             # Extract hyperparameters
             nu = params[0]
@@ -92,8 +132,7 @@ class bants:
             Sm = Psi/(nu-N+1.0)
             
             # Sum log-evidence contributions by each data point
-            mvt = tfd.MultivariateStudentTLinearOperator(df=nu,loc=Mt,scale=tf.linalg.LinearOperatorLowerTriangular(Sm))
-            lnE = np.sum(np.log(mvt.prob(df.values).eval()),axis=0)
+            lnE = np.sum(self.tD_logpdf(df,nu-N+1.0,Mt,Sm),axis=0)
             
             # Output corresponding value to minimise
             return -lnE
@@ -120,13 +159,14 @@ class bants:
         # Make training data available to class object
         self.train_df = train_df
 
-        # Find dimensions of dataset
-        self.N = len(train_df.columns)
+        # Find dimensions and number of samples in dataset
+        self.Nd = len(train_df.columns)
+        self.Ns = len(train_df.index)
 
         # If number of columns is more than 1 then check to see if kernel types have been specified. If not then
         # simply set each column to the 'SquareExp' default option. 
-        if (self.N > 1) and (len(self.column_kernel_types) > 1): 
-            self.column_kernel_types = self.column_kernel_types*self.N
+        if (self.Nd > 1) and (len(self.column_kernel_types) > 1): 
+            self.column_kernel_types = self.column_kernel_types*self.Nd
         
         # If 'AR-GP' network then run appropriate optimisation subroutine
         if self.net_type == 'AR-GP':
